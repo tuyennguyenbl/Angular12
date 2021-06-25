e9d040ccee5fdd92c6a15626fae4d3d886e016ef
tuyennguyenbl <tuyennguyenbl@gmail.com>
2021-06-25 09:47:32 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public name ="Tuyen Nguyen";
-  public age =15;
+  public age =25;
   constructor() { }
 
   ngOnInit(): void {
